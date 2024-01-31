74483174162c0e435e43c71a7b2bd5e6e3d39003
Glauber Funez <glauber.funez@asaas.com.br>
2024-01-31 16:27:42 -0300

    projeto criado

---
+++ src/test/java/com/glauber/CrudSpringAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glauber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringAulaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
