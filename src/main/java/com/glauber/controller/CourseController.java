37543b1132ca6a9213d66d7f5591271e768ee9ba
Glauber Funez <glauber.funez@asaas.com.br>
2024-01-31 17:35:45 -0300

    criando rota courses

---
+++ src/main/java/com/glauber/controller/CourseController.java
@@ -0,0 +1,23 @@
+package com.glauber.controller;
+
+import com.glauber.model.Course;
+import com.glauber.repository.CourseRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses") // Defina a rota
+@AllArgsConstructor // instancia todos os construtores
+public class CourseController {
+
+    final private CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+}
